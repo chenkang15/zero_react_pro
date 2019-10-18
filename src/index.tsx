a85359f049df6c032525d7102da3a1a5fc42981c
chenkang <ckchenkang_v@didichuxing.com>
2019-10-18 20:54:04 +0800

    commit

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,4 @@ import ReactDom from "react-dom";
 import './index.less'
 import { Hello } from './test'
 new Promise((resolve, reject) => {setTimeout(() => {resolve('????')}, 5000)}).then((res) => {console.log(res);})
-ReactDom.render( <Hello str={'???'}/>,document.getElementById("root"));
+ReactDom.render( <Hello str={'????'}/>,document.getElementById("root"));
